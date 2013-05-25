72b64e1564880d78cac736ccf3ba89ba689e2867
Markus Herb <mail@markusherb.de>
2013-05-25 14:46:31 +0200

    Update documentation in CheckpointTsunamiScenario

--- src/scenarios/SWE_CheckpointTsunamiScenario.hh
+++ src/scenarios/SWE_CheckpointTsunamiScenario.hh
@@ -129,11 +129,22 @@ protected:
         assert(x_step > 0.0); assert(y_step > 0.0);
     }
     
+    /// Abort execution with netCDF error message
+    /**
+     * @param status The error status returned by a netCDF function call
+     */
     void handleNetCDFError(int status) {
         std::cerr << "NetCDF Error: " << nc_strerror(status) << std::endl;
         exit(status);
     }
     
+    /// Calculate the nearest cell index for a position inside a domain for a single dimension
+    /**
+     * @param relativePosition The position relative to the origin of the domain
+     * @param stepWidth The assumed step width between cells
+     * @param length The number of cells in the file
+     * @return The index of the nearest cell in the domain
+     */
     size_t getIndex1D(float relativePosition, float stepWidth, size_t length) {
         size_t index;
         
@@ -150,6 +161,12 @@ protected:
         return index;
     }
     
+    /// Calculate the nearest cell index for a position inside a domain for a two dimensions
+    /**
+     * @param x The absolute x position in the domain
+     * @param y The absolute y position in the domain
+     * @param index Pointer to where the grid indices should be written
+     */
     void getIndex(float x, float y, size_t index[2]) {
         // Y index
         index[0] = getIndex1D(y-getBoundaryPos(BND_BOTTOM), y_step, y_len);
@@ -157,12 +174,20 @@ protected:
         index[1] = getIndex1D(x-getBoundaryPos(BND_LEFT), x_step, x_len);
     }
     
-    float readFloatValue(int varid, float x, float y, int isTimeDependend = 1) {
+    /// Read the floating point value from a variable at a specified position
+    /**
+     * @param varid The NetCDF variable ID from which to read
+     * @param x The absolute x position in the domain
+     * @param y The absolute y position in the domain
+     * @param isTimeDependent Whether the variable is a function of the time (true) or not (false)
+     * @return The float value at the specified position
+     */
+    float readFloatValue(int varid, float x, float y, bool isTimeDependent = true) {
         // Index array for reading values from NetCDF
-        int indexLength = isTimeDependend ? 3 : 2;
+        int indexLength = isTimeDependent ? 3 : 2;
         
         size_t index[indexLength];
-        if(isTimeDependend) {
+        if(isTimeDependent) {
             getIndex(x, y, (index+1));
             // We want the value at the latest time step
             index[0] = time_len - 1; 
@@ -176,6 +201,11 @@ protected:
         return value;
     }
     
+    /// Read and decode the boundary type from a NetCDF attribute
+    /**
+     * @param name The name of the attribute
+     * @return The boundary type encoded by the attribute
+     */
     BoundaryType readBoundaryType(const char *name) {
         int status;
         size_t length;
@@ -222,7 +252,10 @@ protected:
     }
 
 public:
-
+    /// Constructor
+    /**
+     * @param checkpointFile The file name of the checkpoint file to be loaded
+     */
     SWE_CheckpointTsunamiScenario(std::string checkpointFileName)
     : SWE_Scenario() {
         loadInputFiles(checkpointFileName);
@@ -256,7 +289,7 @@ public:
             return readFloatValue(hu_id, x, y) / height;
         return 0.0;
     };
-   
+    
     /**
      * @return velocity in y-direction at pos
      */
@@ -267,11 +300,20 @@ public:
         return 0.0;
     };
     
+    /// Get the number of grid cells in x- and y-direction
+    /**
+     * @param nX Pointer to where the number of grid cells in x-direction should be written
+     * @param nY Pointer to where the number of grid cells in y-direction should be written
+     */
     void getNumberOfCells(int &nX, int &nY) {
         nX = x_len;
         nY = y_len;
     }
     
+    /// Get the total number of checkpoints to be written in the simulation
+    /**
+     * @return total number of checkpoints
+     */
     int getNumberOfCheckpoints() {
         int numberOfCheckpoints;
         int status = nc_get_att_int(file_id, NC_GLOBAL, "numberOfCheckpoints", &numberOfCheckpoints);
@@ -284,6 +326,11 @@ public:
         return 1;
     }
     
+    /// Read the number and timestep of the last checkpoint from checkpointfile
+    /**
+     * @param checkpoint Pointer to where the checkpoint number should be written
+     * @param time Pointer to where the timestep time shoudl be written
+     */
     void getLastCheckpoint(int &checkpoint, float &time) {
         checkpoint = time_len - 1; // subtract initial zero-timestep
 
@@ -311,9 +358,8 @@ public:
         return 50.0f;
     };
 
+    /// Get the type (e.g. reflecting wall or outflow) of a certain boundary
     /**
-     * Determines the type (e.g. reflecting wall or outflow) of a certain boundary
-     *
      * @param edge The boundary edge
      * @return The type of the specified boundary (e.g. OUTFLOW or WALL)
      */
@@ -327,8 +373,8 @@ public:
         return readBoundaryType("boundaryTypeTop");
     };
     
-    /** Get the boundary positions
-     *
+    /// Get the boundary positions
+    /**
      * @param i_edge which edge
      * @return value in the corresponding dimension
      */
